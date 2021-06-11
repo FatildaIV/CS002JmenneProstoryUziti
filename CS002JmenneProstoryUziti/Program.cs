339f0419d1b050caba475760a6ce597b9747bbc1
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 11:07:29 +0200

    Add files via upload

---
+++ CS002JmenneProstoryUziti/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CS002JmenneProstoryUziti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Karel");
+            Console.ReadKey();
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine("Hofman");
+            Console.ReadKey();
+        }
+    }
+}
